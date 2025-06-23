15d1eafef1516c8cd97e0b42eac5da571dbc9f3d
skwy <qiboda00@gmail.com>
2025-06-23 22:45:15 +0800

    add package dep

--- src/new_module.ts
+++ src/new_module.ts
@@ -63,7 +63,7 @@ target(module_name)
     set_symbols("debug")
     set_strip("all")
 
-    add_packages("glfw", "glm", "stb", "spdlog", "tracy")
+    add_packages("glfw", "glm", "stb", "spdlog", "tracy", "rpmalloc", "tbb")
     add_packages("vulkansdk")
 
     add_deps("core")
